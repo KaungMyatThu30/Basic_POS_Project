510b75a7db366a255fd2cb2c334659bf779a19f1
thantsinwin8917 <thantsinwin8917@gmail.com>
2026-01-24 19:55:21 +0700

    sidebar

--- basic_pos/src/components/Sidebar.jsx
+++ basic_pos/src/components/Sidebar.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-
+ 
 const Sidebar = () => (
   <aside className="sidebar">
     <div className="brand">
-      {/* Logo Icon */}
       <svg
         className="svg-icon"
         style={{ width: 28, height: 28 }}
@@ -14,7 +13,7 @@ const Sidebar = () => (
       </svg>
       Nexus POS
     </div>
-
+ 
     <nav className="nav-menu">
       <NavLink
         to="/"
@@ -22,7 +21,6 @@ const Sidebar = () => (
           isActive ? "nav-link active" : "nav-link"
         }
       >
-        {/* Dashboard Icon */}
         <svg className="svg-icon" viewBox="0 0 24 24">
           <rect x="3" y="3" width="7" height="7" />
           <rect x="14" y="3" width="7" height="7" />
@@ -31,14 +29,13 @@ const Sidebar = () => (
         </svg>
         Overview
       </NavLink>
-
+ 
       <NavLink
         to="/journal"
         className={({ isActive }) =>
           isActive ? "nav-link active" : "nav-link"
         }
       >
-        {/* Journal/Receipt Icon */}
         <svg className="svg-icon" viewBox="0 0 24 24">
           <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z" />
           <polyline points="14 2 14 8 20 8" />
@@ -51,5 +48,5 @@ const Sidebar = () => (
     </nav>
   </aside>
 );
-
+ 
 export default Sidebar;
